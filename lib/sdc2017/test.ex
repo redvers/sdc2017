0a050d3fbba7c6be3d4778c060830fd10e5457e7
Red Davies <red@evil.red>
2017-10-17 02:28:23 +0000

    Noddy Shakespeare thing

--- lib/sdc2017/test.ex
+++ lib/sdc2017/test.ex
@@ -3,27 +3,44 @@ defmodule SDC2017.Test do
   use GenServer
 
   def start_link(badgeid) do
-    GenServer.start_link(__MODULE__, %{id: badgeid}, [])
+    GenServer.start_link(__MODULE__, %{id: badgeid, tbox: SDC2017.Tbox.cls}, [])
   end
 
   def init(state = %{id: badgeid}) do
     Registry.register(:badgeapps, badgeid, __MODULE__)
-    {:ok, state}
+    {:ok, Map.put(state, :culture, parse_culture())}
   end
 
   def handle_cast(:die, state) do
     {:stop, :normal, state}
   end
 
-  def handle_call(ii = {:payload, payload}, _, state) do
-    bindata = SDC2017.Tbox.cls
-    |> SDC2017.Tbox.print(%{x: 0, y: 0}, inspect(__MODULE__))
-    |> SDC2017.Tbox.print(%{x: 0, y: 3}, "lastevent")
-    |> SDC2017.Tbox.print(%{x: 0, y: 4}, inspect(payload))
+  def handle_call(ii = {:payload, payload}, _, state = %{tbox: tbox, culture: [line | rest]}) do
+    IO.inspect(line)
+    newtbox = 
+    case line do
+      [""] -> SDC2017.Tbox.crlf(tbox)
+      words -> t =
+               word = Enum.reject(words, &(&1 == ""))
+                      |> Enum.join(" ")
+               SDC2017.Tbox.print(tbox, word)
+
+    end
+#    |> SDC2017.Tbox.print(inspect(payload))
     |> SDC2017.Tbox.pp
-    |> SDC2017.OLED.render
+    IO.inspect(newtbox)
+    bindata = SDC2017.OLED.render(newtbox)
     Logger.debug("#{inspect(__MODULE__)}: #{inspect(ii)}")
-    {:reply, bindata, state}
+    nstate = Map.put(state, :culture, rest)
+    {:reply, bindata, Map.put(nstate, :tbox, newtbox)}
   end
+
+  def parse_culture do
+    File.read!("culture/son.txt")
+    |> String.split("\n")
+    |> Enum.map(&(String.split(&1, " ")))
+  end
+
+
 
 end
